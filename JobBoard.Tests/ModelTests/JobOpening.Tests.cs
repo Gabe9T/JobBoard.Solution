a0d3ee8ca3a90a2df2c60e59c2dc2bd8c6f4852e
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-23 09:33:59 -0800

    passed constructor test
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>

--- JobBoard.Tests/ModelTests/JobOpening.Tests.cs
+++ JobBoard.Tests/ModelTests/JobOpening.Tests.cs
@@ -1,17 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using ToDoList.Models;
+using JobBoard.Models;
 using System;
 
 namespace JobBoard.TestTools
 {
   [TestClass]
-  public class JobBoardTests
+  public class JobOpeningTests
   {
     [TestMethod]
-    //
+    public void JobOpeningConstructor_CreatesInstanceOfJobOpening_JobOpening()
     {
-
+      JobOpening newJobOpening = new JobOpening();
+      Assert.AreEqual(typeof(JobOpening), newJobOpening.GetType());
     }
 
   }
