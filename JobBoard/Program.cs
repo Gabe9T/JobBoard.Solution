73678e5041b3160bf33a099101a383beaccae076
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-23 15:52:44 -0800

    add styling
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>

--- JobBoard/Program.cs
+++ JobBoard/Program.cs
@@ -14,7 +14,7 @@ namespace JobBoard
       WebApplication app = builder.Build();
 
       app.UseHttpsRedirection();
-
+      app.UseStaticFiles();
       app.UseRouting();
 
       app.MapControllerRoute(
