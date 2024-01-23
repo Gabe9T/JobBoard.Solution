92ba2e0e54d66c7b074ce2bfbe8b732d3bc6996b
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-23 09:42:19 -0800

    pass test to get title
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>

--- JobBoard.Tests/ModelTests/JobOpening.Tests.cs
+++ JobBoard.Tests/ModelTests/JobOpening.Tests.cs
@@ -11,8 +11,17 @@ namespace JobBoard.TestTools
     [TestMethod]
     public void JobOpeningConstructor_CreatesInstanceOfJobOpening_JobOpening()
     {
-      JobOpening newJobOpening = new JobOpening();
+      JobOpening newJobOpening = new JobOpening("Dog Walker");
       Assert.AreEqual(typeof(JobOpening), newJobOpening.GetType());
+    }
+
+    [TestMethod]
+    public void GetTitle_ReturnsTitle_String()
+    {
+      string title = "Dog Walker";
+      JobOpening newJob = new JobOpening(title);
+      string result = newJob.Title;
+      Assert.AreEqual(title, result);
     }
 
   }
