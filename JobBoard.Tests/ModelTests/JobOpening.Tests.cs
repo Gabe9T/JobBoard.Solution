cbbb245923dd591c9a97e1995646b4b95f688136
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-23 09:54:01 -0800

    pass test to get description
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>

--- JobBoard.Tests/ModelTests/JobOpening.Tests.cs
+++ JobBoard.Tests/ModelTests/JobOpening.Tests.cs
@@ -11,7 +11,7 @@ namespace JobBoard.TestTools
     [TestMethod]
     public void JobOpeningConstructor_CreatesInstanceOfJobOpening_JobOpening()
     {
-      JobOpening newJobOpening = new JobOpening("Dog Walker");
+      JobOpening newJobOpening = new JobOpening("Dog Walker", "desc");
       Assert.AreEqual(typeof(JobOpening), newJobOpening.GetType());
     }
 
@@ -19,7 +19,7 @@ namespace JobBoard.TestTools
     public void GetTitle_ReturnsTitle_String()
     {
       string title = "Dog Walker";
-      JobOpening newJob = new JobOpening(title);
+      JobOpening newJob = new JobOpening(title, "desc");
       string result = newJob.Title;
       Assert.AreEqual(title, result);
     }
@@ -27,11 +27,21 @@ namespace JobBoard.TestTools
     public void SetTitle_SetsValueOfTitle_String()
     {
       string title = "Dog Walker";
-      JobOpening newJob = new JobOpening(title);
+      JobOpening newJob = new JobOpening(title, "desc");
       string updatedTitle = "Line Cook";
       newJob.Title = updatedTitle;
       string result = newJob.Title;
       Assert.AreEqual(updatedTitle, result);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnDescription_String()
+    {
+      string title = "Dog Walker";
+      string description = "I walk dogs.";
+      JobOpening newJob = new JobOpening(title, description);
+      string result = newJob.Description;
+      Assert.AreEqual(description, result);
     }
 
   }
